5a2f2a8287954f957ef56e36dbff2cbe5f3bdade
Laura Hoffman <lhoffmanwg@gmail.com>
2019-04-19 02:59:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def dictionary
  substitution_hash = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
  
@@ -11,6 +12,7 @@ def word_substituter(tweet)
     dictionary.fetch(word.downcase, word)
   end  
   .join(" ") 
+  binding.pry
 end  
 
 def bulk_tweet_shortener(tweets_ary)
